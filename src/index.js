1590a3f5d6b918f2085c20b0b4ed7b27cf619b92
Ankur <ankur@thb.co.in>
2018-08-09 08:54:24 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyDeb_qecgWCB_eUfuZNxOdM1Th0OJu9p3U'
+
+const App = () => {
+   return <div> Hi! </div>
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
